6d229869f05b24fc92b41452f0c173aa08cc0941
Nostalgic11 <tinejames1995@gmail.com>
2022-09-19 19:32:31 +0000

    To do list

---
+++ TODO-List.js
@@ -0,0 +1,55 @@
+// Creer un bouton "supprimer pour les elements (li) de notre (ul)."
+var myNodelist = document.getElementsByTagName("li");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+ span.className = "close";
+ span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Cliquer sur le bouton supprimer afin d'appliquer la suppression.
+  var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Ajouter un "checked" symbol quand on clique sur un element de la liste.
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Creer une nouvelle liste en cliquant sur "Ajouter".
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
